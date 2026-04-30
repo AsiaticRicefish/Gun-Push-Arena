041cef3c29dcaeb79da760b63a540e909111959f
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2026-04-30 15:33:09 +0900

    [Feat] 인증 관리
    접속했을 때 각 고유 ID를 저장할 수 있도록 관리

---
+++ Assets/Scripts/Core/AuthManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using Firebase.Auth;
+using Firebase.Extensions;
+using System.Threading.Tasks;
+
+public class AuthManager : GlobalSingleton<AuthManager>
+{
+    private FirebaseAuth auth;
+
+    // 로그인한 사용자의 고유 ID를 저장하는 프로퍼티입니다. 로그인 상태를 확인할 때 사용됩니다.
+    public string UserId { get; private set; }
+    // UserId가 null 또는 빈 문자열이 아닌 경우 로그인된 상태로 간주하는 프로퍼티입니다. 로그인 여부를 쉽게 확인할 수 있도록 도와줍니다.
+    public bool IsLoggedIn => !string.IsNullOrEmpty(UserId);
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (Instance != this) return;
+    }
+
+    /// <summary>
+    /// Firebase 초기화가 완료될 때까지 대기한 후, Firebase Authentication을 초기화하고 익명 로그인 시도를 합니다.
+    /// </summary>
+    private async void Start()
+    {
+        await WaitForFirebase();
+        InitAuth();
+        await SignInAnonymously();
+    }
+
+    /// <summary>
+    /// Firebase 초기화가 완료될 때까지 대기하는 비동기 메서드입니다. 
+    /// FirebaseManager의 InReady 속성을 확인하여 초기화가 완료되었는지 판단합니다.
+    /// Firebase는 비동기 초기화라서 바로 Auth 쓰면 터질 수 있습니다. 그래서 초기화 완료될 때까지 대기하는 로직이 필요합니다.
+    /// </summary>
+    /// <returns></returns>
+    private async Task WaitForFirebase()
+    {
+        while (!FirebaseManager.Instance.InReady)
+        {
+            await Task.Yield();
+        }
+    }
+
+    /// <summary>
+    /// Firebase Authentication 초기화 메서드입니다. FirebaseAuth.DefaultInstance를 사용하여 auth 인스턴스를 초기화하고, 초기화 완료 로그를 출력합니다.
+    /// </summary>
+    private void InitAuth()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        Debug.Log("[Auth] Firebase Authentication initialized.");
+    }
+
+    /// <summary>
+    /// 익명 로그인 시도 메서드입니다. SignInAnonymouslyAsync() 메서드를 사용하여 익명 로그인을 시도하고, 로그인 성공 시 UserId를 저장하고 로그를 출력합니다. 로그인 실패 시 예외를 캐치하여 에러 로그를 출력합니다.
+    /// </summary>
+    /// <returns></returns>
+    private async Task SignInAnonymously()
+    {
+        try
+        {
+            var result = await auth.SignInAnonymouslyAsync();
+            UserId = result.User.UserId;
+
+            Debug.Log($"[Auth] Login Success / UID: {UserId}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"[Auth] Login Failed: {e}");
+        }
+    }
+}
