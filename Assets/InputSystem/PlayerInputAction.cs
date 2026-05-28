4ef4f80e5da2be235eb53c4e258473cc4af6c69a
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2026-05-28 17:17:23 +0900

    [Feat] 플레이어 점프 추가

--- Assets/InputSystem/PlayerInputAction.cs
+++ Assets/InputSystem/PlayerInputAction.cs
@@ -100,6 +100,15 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""195d88f2-12a0-44d3-95b6-2bb1b895505a"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -157,6 +166,17 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a83860ab-8e4b-40f1-af49-b6c4fc895309"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";PC;Mobile"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -194,6 +214,7 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
     }
 
     ~@PlayerInputAction()
@@ -275,6 +296,7 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -290,6 +312,10 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Move".
         /// </summary>
         public InputAction @Move => m_Wrapper.m_Player_Move;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Jump".
+        /// </summary>
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -319,6 +345,9 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
             @Move.started += instance.OnMove;
             @Move.performed += instance.OnMove;
             @Move.canceled += instance.OnMove;
+            @Jump.started += instance.OnJump;
+            @Jump.performed += instance.OnJump;
+            @Jump.canceled += instance.OnJump;
         }
 
         /// <summary>
@@ -333,6 +362,9 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
             @Move.started -= instance.OnMove;
             @Move.performed -= instance.OnMove;
             @Move.canceled -= instance.OnMove;
+            @Jump.started -= instance.OnJump;
+            @Jump.performed -= instance.OnJump;
+            @Jump.canceled -= instance.OnJump;
         }
 
         /// <summary>
@@ -406,5 +438,12 @@ public partial class @PlayerInputAction: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnMove(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Jump" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnJump(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,10 @@ using System.Collections;
 public class PlayerController : NetworkBehaviour
 {
     private PlayerInputAction input;
+    private Rigidbody2D rb;
+    private Collider2D playerCollider;
+    private float serverMoveInput;
+    private bool serverJumpRequested;
 
     // 로그인된 사용자의 UID를 서버로 전송하기 위해 IAuthService 인터페이스를 사용하여 AuthManager에 접근합니다.
     // 이를 통해 PlayerController가 AuthManager에 직접 의존하지 않고도 로그인 정보를 사용할 수 있도록 합니다.
@@ -13,6 +17,9 @@ public class PlayerController : NetworkBehaviour
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private LayerMask groundLayerMask = Physics2D.DefaultRaycastLayers;
+    [SerializeField] private float groundCheckDistance = 0.08f;
 
     [Header("Visual")]
     [SerializeField] private Renderer playerRenderer;
@@ -29,6 +36,8 @@ public class PlayerController : NetworkBehaviour
     private void Awake()
     {
         input = new PlayerInputAction();
+        rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
 
         if (playerRenderer == null)
         {
@@ -106,9 +115,29 @@ public class PlayerController : NetworkBehaviour
         if (!IsOwner) return;
 
         Vector2 moveInput = input.Player.Move.ReadValue<Vector2>();
+        bool jumpPressed = input.Player.Jump.WasPressedThisFrame();
 
         // 입력에 따라 서버로 이동 명령을 보냅니다
-        MoveServerRpc(moveInput);
+        MoveServerRpc(moveInput.x, jumpPressed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsServer || rb == null)
+        {
+            return;
+        }
+
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = serverMoveInput * moveSpeed;
+
+        if (serverJumpRequested && IsGrounded())
+        {
+            velocity.y = jumpForce;
+        }
+
+        rb.linearVelocity = velocity;
+        serverJumpRequested = false;
     }
 
     /// <summary>
@@ -117,10 +146,32 @@ public class PlayerController : NetworkBehaviour
     /// </summary>
     /// <param name="moveInput"></param>
     [ServerRpc]
-    private void MoveServerRpc(Vector2 moveInput)
+    private void MoveServerRpc(float horizontalInput, bool jumpPressed)
     {
-        Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        serverMoveInput = Mathf.Clamp(horizontalInput, -1f, 1f);
+        serverJumpRequested |= jumpPressed;
+    }
+
+    private bool IsGrounded()
+    {
+        if (playerCollider == null)
+        {
+            return false;
+        }
+
+        Bounds bounds = playerCollider.bounds;
+        Vector2 origin = new Vector2(bounds.center.x, bounds.min.y - groundCheckDistance * 0.5f);
+        Vector2 size = new Vector2(bounds.size.x * 0.8f, groundCheckDistance);
+
+        RaycastHit2D hit = Physics2D.BoxCast(
+            origin,
+            size,
+            0f,
+            Vector2.down,
+            groundCheckDistance,
+            groundLayerMask);
+
+        return hit.collider != null && hit.collider != playerCollider;
     }
 
     /// <summary>
